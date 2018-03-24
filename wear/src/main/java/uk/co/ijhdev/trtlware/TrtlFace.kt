5ecbd585bcdad493a2e765fbb234eaf8e4d51b80
Ian Hayward <ian@ijhdev.co.uk>
2018-03-24 16:52:00 +0000

    Design showing, time date and watch power displayed

--- wear/src/main/java/uk/co/ijhdev/trtlware/TrtlFace.kt
+++ wear/src/main/java/uk/co/ijhdev/trtlware/TrtlFace.kt
@@ -8,17 +8,25 @@ import android.graphics.*
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
-import android.support.v4.content.ContextCompat
+import android.support.wearable.complications.SystemProviders.batteryProvider
 import android.support.wearable.watchface.CanvasWatchFaceService
 import android.support.wearable.watchface.WatchFaceService
 import android.support.wearable.watchface.WatchFaceStyle
 import android.view.*
 import android.widget.Toast
-
+import kotlinx.android.synthetic.main.watchface.*
 import java.lang.ref.WeakReference
 import java.util.Calendar
 import java.util.TimeZone
 import android.widget.TextView
+import org.w3c.dom.Text
+import uk.co.ijhdev.trtlware.R.id.*
+import kotlin.math.roundToInt
+import android.os.BatteryManager
+import android.content.Context.BATTERY_SERVICE
+
+
+
 
 
 
@@ -39,12 +47,6 @@ class TrtlFace : CanvasWatchFaceService() {
     lateinit var watchLayout : View
     var specW: Int = 0
     var specH:Int = 0
-    private val day: TextView? = null
-    private val date: TextView? = null
-    private val month: TextView? = null
-    private val hour: TextView? = null
-    private val minute: TextView? = null
-    private val second: TextView? = null
     private val displaySize = Point()
 
     companion object {
@@ -63,15 +65,6 @@ class TrtlFace : CanvasWatchFaceService() {
     }
 
     override fun onCreateEngine(): Engine {
-
-        var inflater:LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        watchLayout = inflater.inflate(R.layout.watchface, null)
-
-        var display = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        display.getDefaultDisplay()
-        specW = View.MeasureSpec.makeMeasureSpec(displaySize.x, View.MeasureSpec.EXACTLY)
-        specH = View.MeasureSpec.makeMeasureSpec(displaySize.y, View.MeasureSpec.EXACTLY);
-
         return Engine()
     }
 
@@ -129,17 +122,14 @@ class TrtlFace : CanvasWatchFaceService() {
             val resources = this@TrtlFace.resources
             mYOffset = resources.getDimension(R.dimen.digital_y_offset)
 
-            // Initializes background.
-            mBackgroundPaint = Paint().apply {
-                color = ContextCompat.getColor(applicationContext, R.color.background)
-            }
+            // min_time  = mintime as TextView
+          //  second_time = secondtime as TextView
+          //
+            var inflater:LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            watchLayout = inflater.inflate(R.layout.watchface, null)
+            var display = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            display.defaultDisplay.getSize(displaySize)
 
-            // Initializes Watch Face.
-            mTextPaint = Paint().apply {
-                typeface = NORMAL_TYPEFACE
-                isAntiAlias = true
-                color = ContextCompat.getColor(applicationContext, R.color.digital_text)
-            }
         }
 
         override fun onDestroy() {
@@ -163,10 +153,10 @@ class TrtlFace : CanvasWatchFaceService() {
         override fun onAmbientModeChanged(inAmbientMode: Boolean) {
             super.onAmbientModeChanged(inAmbientMode)
             mAmbient = inAmbientMode
-
-            if (mLowBitAmbient) {
-                mTextPaint.isAntiAlias = !inAmbientMode
-            }
+//
+//            if (mLowBitAmbient) {
+//                mTextPaint.isAntiAlias = !inAmbientMode
+//            }
 
             // Whether the timer should be running depends on whether we're visible (as well as
             // whether we're in ambient mode), so we may need to start or stop the timer.
@@ -195,25 +185,34 @@ class TrtlFace : CanvasWatchFaceService() {
         }
 
         override fun onDraw(canvas: Canvas, bounds: Rect) {
-            // Draw the background.
-            if (mAmbient) {
-                canvas.drawColor(Color.BLACK)
-            } else {
-                canvas.drawRect(
-                        0f, 0f, bounds.width().toFloat(), bounds.height().toFloat(), mBackgroundPaint)
-            }
 
             // Draw H:MM in ambient mode or H:MM:SS in interactive mode.
             val now = System.currentTimeMillis()
             mCalendar.timeInMillis = now
+            val date : TextView = watchLayout.findViewById(R.id.date_number)
+            date.text = String.format("%02d/%02d", mCalendar.get(Calendar.DAY_OF_MONTH), mCalendar.get(Calendar.MONTH) + 1)
+            val hour : TextView = watchLayout.findViewById(R.id.hourtime)
+            hour.text = String.format("%02d", mCalendar.get(Calendar.HOUR_OF_DAY))
+            val min : TextView = watchLayout.findViewById(R.id.mintime)
+            min.text = String.format("%02d", mCalendar.get(Calendar.MINUTE))
 
-            val text = if (mAmbient)
-                String.format("%d:%02d", mCalendar.get(Calendar.HOUR),
-                        mCalendar.get(Calendar.MINUTE))
-            else
-                String.format("%d:%02d:%02d", mCalendar.get(Calendar.HOUR),
-                        mCalendar.get(Calendar.MINUTE), mCalendar.get(Calendar.SECOND))
-            canvas.drawText(text, mXOffset, mYOffset, mTextPaint)
+            if (!mAmbient) {
+                val second : TextView = watchLayout.findViewById(R.id.secondtime)
+                second.text = String.format("%02d", mCalendar.get(Calendar.SECOND))
+            }
+            val bm = getSystemService(BATTERY_SERVICE) as BatteryManager
+            val batLevel = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+
+            val watch : TextView = watchLayout.findViewById(R.id.watch_power)
+            watch.text = batLevel.toString() + " %"
+
+            watchLayout.measure(specW, specH);
+            watchLayout.layout(0, 0, watchLayout.measuredWidth, watchLayout.measuredHeight)
+            canvas.save()
+           // canvas.drawColor(Color.BLACK)
+            canvas.translate(mXOffset,mYOffset - 40)
+            watchLayout.draw(canvas)
+            canvas.restore()
         }
 
         override fun onVisibilityChanged(visible: Boolean) {
@@ -257,21 +256,17 @@ class TrtlFace : CanvasWatchFaceService() {
             // Load resources that have alternate values for round watches.
             val resources = this@TrtlFace.resources
             val isRound = insets.isRound
-            mXOffset = resources.getDimension(
-                    if (isRound)
-                        R.dimen.digital_x_offset_round
-                    else
-                        R.dimen.digital_x_offset
-            )
-
-            val textSize = resources.getDimension(
-                    if (isRound)
-                        R.dimen.digital_text_size_round
-                    else
-                        R.dimen.digital_text_size
-            )
-
-            mTextPaint.textSize = textSize
+            if (insets.isRound()) {
+                // Shrink the face to fit on a round screen
+                mYOffset = displaySize.x * 0.1f
+                displaySize.y -= 2 * mXOffset.roundToInt()
+                displaySize.x -= 2 * mXOffset.roundToInt()
+            } else {
+                mXOffset = 0f
+            }
+            specW = View.MeasureSpec.makeMeasureSpec(displaySize.x, View.MeasureSpec.EXACTLY)
+            specH = View.MeasureSpec.makeMeasureSpec(displaySize.y, View.MeasureSpec.EXACTLY)
+          //  mTextPaint.textSize = textSize
         }
 
         /**
